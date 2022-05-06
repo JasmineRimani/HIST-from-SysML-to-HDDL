1b800607894f68c685c44102cd514991c006f786
Jasmine Rimani <49746299+JasmineRimani@users.noreply.github.com>
2022-05-06 12:06:51 +0200

    Add files via upload

--- SysML-HDDL translation/Code/xml_parsing.py
+++ SysML-HDDL translation/Code/xml_parsing.py
@@ -28,7 +28,8 @@ class XML_parsing():
         
     # Parse the XML File with BeautifulSoap    
     def XML_ActiveParsing(self):
-        # Requirement list for the specific domain file
+        "-------- Initial Variables Set-up --------"
+        # HDDL Requirement list for the specific domain file
         requirement_list_domain_file = []
         # Packages list 
         package_list = []
@@ -36,65 +37,84 @@ class XML_parsing():
         hddl_type_list = []
         # Actors list
         actor_list = []       
-        # HighLevel UseCase list - Tasks
+        # Tasks Lists
         task_list = []
         # Edges List
         edge_list = []
-        # Feedback edge list
+        # Feedback edge list - it saves the edges that are ill defined to a different list that the normal edges
         edge_list_feedback = []
         # Log file general entries
         log_file_general_entries = []
         # Dependencies in the UseCase
         dependencies_list = []
-        # Constraints in a Package
+        # Constraints in a Package: 
+            # the constraints are or Task parameters or Problem file initial conditions
         b_ownedRules_from_package = []
 
         # Log File init
         log_file_general_entries.append('Log errors and warnings during parsing: \n')
         log_file_general_entries.append('------------------------------------------------- \n')
+       
+        "-------- Actual Code --------"
         # Passing the stored data inside the beautifulsoup parser, 
         # storing the returned object in a variable - that is the main object to unpack
 
-        Bs_data = BeautifulSoup(self.file, "xml")
+        SysML_data = BeautifulSoup(self.file, "xml")
         
         # How to use BeautifulSoup:
             # Finding all instances of tag - You get a list with all the instances with that tag:
-                # b_node = Bs_data.find_all('node')
+                # b_node = SysML_data.find_all('node')
                 # If the tags are nested - you get each tag one after the other from the order of the file
         
             # Find all the packaged elements
                 # You can divide the packaged elements per Folder - so that you don't have to parse useless information like the ones for the Mission
         
-        b_packagedElement = Bs_data.find_all('packagedElement') 
-        b_nestedClassifier = Bs_data.find_all('nestedClassifier') 
+        b_packagedElement = SysML_data.find_all('packagedElement') 
+        b_nestedClassifier = SysML_data.find_all('nestedClassifier') 
         self.xml_parsing_output["b_packagedElement"] = b_packagedElement
         
 
-        for index,ii in enumerate(b_packagedElement):
+        for index,element in enumerate(b_packagedElement):
             
             # Isolate the "xmi:type="uml:Package" " --> e.g. b_packagedElement[0]['xmi:type']
-            if ii['xmi:type'] == 'uml:Package':
-                package_list.append({"name": ii['name'], "xmi:id":ii['xmi:id']})    
+            if element['xmi:type'] == 'uml:Package':
+                package_list.append({"name": element['name'], "xmi:id":element['xmi:id']})    
             
             # Isolate the classes - ':types' in HDDL
-            if ii['xmi:type'] == 'uml:Class' and ii.parent["name"] == 'Types':
-                hddl_type_list.append({"name": ii['name'].replace(" ", ""), "xmi:id":ii['xmi:id']})
+            if element['xmi:type'] == 'uml:Class' and element.parent["name"] == 'Types':
+                hddl_type_list.append({"name": element['name'].replace(" ", ""), "xmi:id":element['xmi:id']})
 
             # Isolate Actors:
-            if ii['xmi:type'] == 'uml:Actor':
-                actor_list.append({"name": ii['name'], "xmi:id":ii['xmi:id']})
+            if element['xmi:type'] == 'uml:Actor':
+                actor_list.append({"name": element['name'], "xmi:id":element['xmi:id']})
             
             # Isolate Usecases that are packegedElements --> You get your tasks name, however you still need your parameters
-            if ii['xmi:type'] == 'uml:UseCase':
-                task_list.append({"name": ii['name'], "xmi:id":ii['xmi:id'], "parameters": []})   
+            if element['xmi:type'] == 'uml:UseCase':
+                task_list.append({"name": element['name'], "xmi:id":element['xmi:id'], "parameters": []})   
                 
             # Isolate Packaged Constraints:
-            if ii['xmi:type'] == 'uml:Constraint':
-                b_ownedRules_from_package.append(ii)                
+            if element['xmi:type'] == 'uml:Constraint':
+                # if element.descent
+                for child in element.children:
+                    # If the tag has been defined as Parameter Spec
+                    try:
+                        if not isinstance(child, str) and child.attrs['name'] == 'ParamSpec':
+                            element_type = child.body.contents[0]
+                            element['name'] = '{} - {}'.format(element['name'], element_type)
+                            b_ownedRules_from_package.append(element) 
+                        # If the tag has been defined as constraintSpec
+                        if not isinstance(child, str) and child.attrs['name'] == 'constraintSpec':
+                            b_ownedRules_from_package.append(element) 
+                        else:
+                            pass
+                    except:
+                        pass
+                
+                # b_ownedRules_from_package.append(element)                
 
-        for index,ii in enumerate(b_nestedClassifier):
-            if ii['xmi:type'] == 'uml:Class' and ii.parent.parent["name"] == 'Types':
-                hddl_type_list.append({"name": ii['name'].replace(" ", ""), "xmi:id":ii['xmi:id'], "parent": ii.parent["name"]})
+        for index,classifier in enumerate(b_nestedClassifier):
+            if classifier['xmi:type'] == 'uml:Class' and classifier.parent.parent["name"] == 'Types':
+                hddl_type_list.append({"name": classifier['name'].replace(" ", ""), "xmi:id":classifier['xmi:id'], "parent": classifier.parent["name"]})
                 
 
         self.xml_parsing_output["b_package_list"] = package_list
@@ -104,52 +124,52 @@ class XML_parsing():
         
         
         # Find all the constraints tag = ownedRule and xmi:type="uml:Constraint"
-        b_ownedRules = Bs_data.find_all('ownedRule') 
+        b_ownedRules = SysML_data.find_all('ownedRule') 
         self.xml_parsing_output["b_ownedRules"] = b_ownedRules + b_ownedRules_from_package
         
         # Find all the edges
-        b_edges = Bs_data.find_all('edge')    
+        b_edges = SysML_data.find_all('edge')    
         self.xml_parsing_output["b_edges"] = b_edges
         # Map all the edges
-        for index,ii in enumerate(b_edges):
+        for index,edge in enumerate(b_edges):
             try:
-                edge_list.append({"xmi:id":ii['xmi:id'], "input":ii['source'], "output":ii['target']}) 
+                edge_list.append({"xmi:id":edge['xmi:id'], "input":edge['source'], "output":edge['target']}) 
             except:
                 # if you can't find one of the ends of the edge: save the edge in the edge list and in the feedback log
                 if self.debug == 'on' :
-                    print('Check your model! Edge id:{} is ill defined! It is probably missing an input or an output'.format(ii['xmi:id']))
+                    print('Check your model! Edge id:{} is ill defined! It is probably missing an input or an output'.format(edge['xmi:id']))
                 # Add this to the future Log File
-                log_file_general_entries.append('\t\t Check your model! Edge id:{} is ill defined! It is probably missing an input or an output \n'.format(ii['xmi:id']))
-                if ii.has_attr('source'):
-                    edge_list.append({"xmi:id":ii['xmi:id'], "input":ii['source'], "output":''}) 
-                    edge_list_feedback.append({"xmi:id":ii['xmi:id'], "input":ii['source'], "output":''}) 
-                if ii.has_attr('target'):
-                    edge_list.append({"xmi:id":ii['xmi:id'], "input":' ', "output":['target']}) 
-                    edge_list_feedback.append({"xmi:id":ii['xmi:id'], "input":' ', "output":['target']}) 
+                log_file_general_entries.append('\t\t Check your model! Edge id:{} is ill defined! It is probably missing an input or an output \n'.format(edge['xmi:id']))
+                if edge.has_attr('source'):
+                    edge_list.append({"xmi:id":edge['xmi:id'], "input":edge['source'], "output":''}) 
+                    edge_list_feedback.append({"xmi:id":edge['xmi:id'], "input":edge['source'], "output":''}) 
+                if edge.has_attr('target'):
+                    edge_list.append({"xmi:id":edge['xmi:id'], "input":' ', "output":['target']}) 
+                    edge_list_feedback.append({"xmi:id":edge['xmi:id'], "input":' ', "output":['target']}) 
             
         self.xml_parsing_output["edge_list"] = edge_list
         #Find the dependencies
-        for index,ii in enumerate(b_packagedElement):
-            if ii['xmi:type'] == 'uml:Dependency' or ii['xmi:type'] == 'uml:Realization':
-                dependencies_list.append({"xmi:id":ii['xmi:id'], "input":ii['supplier'], "output":ii['client']})        
+        for index,connector in enumerate(b_packagedElement):
+            if connector['xmi:type'] == 'uml:Dependency' or connector['xmi:type'] == 'uml:Realization':
+                dependencies_list.append({"xmi:id":connector['xmi:id'], "input":connector['supplier'], "output":connector['client']})        
         
         self.xml_parsing_output["dependencies_list"] = dependencies_list
         # Find all the comments
-        b_comments = Bs_data.find_all('ownedComment') 
+        b_comments = SysML_data.find_all('ownedComment') 
         self.xml_parsing_output["b_comments"] = b_comments
         # Find all the nodes
-        b_nodes = Bs_data.find_all('node')
+        b_nodes = SysML_data.find_all('node')
         self.xml_parsing_output["b_nodes"] = b_nodes
         
         # Domain file instances analysis  
-        for ii in b_comments:
+        for comment in b_comments:
             # The requirements for the HDDL domain file are included as comment in the UseCase
-            if ii.parent['name'] == 'UseCase' and "Requirements" in ii.body.contents[0]:
-                comment_body = ii.body.string
-                for jj in self.hddl_requirements_list:
+            if comment.parent['name'] == 'UseCase' and "Requirements" in comment.body.contents[0]:
+                comment_body = comment.body.string
+                for requirement in self.hddl_requirements_list:
                     # avoid case-sensitivity 
-                    if jj.lower() in comment_body.lower() and jj.lower() not in requirement_list_domain_file:
-                        requirement_list_domain_file.append(jj.lower())
+                    if requirement.lower() in comment_body.lower() and requirement.lower() not in requirement_list_domain_file:
+                        requirement_list_domain_file.append(requirement.lower())
       
         self.xml_parsing_output["requirement_list_domain_file"] = requirement_list_domain_file
         self.xml_parsing_output["log_file_general_entries"] = log_file_general_entries
